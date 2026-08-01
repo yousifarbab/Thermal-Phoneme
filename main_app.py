8e0f1c92e875a308311311ca52ca6dc7bc146aa8
yousifarbab <yousifarbab91@gmail.com>
2026-08-01 16:42:56 +0400

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,3 +1,6 @@
+import numpy as np
+import plotly.graph_objects as go
+
 # تحليل الزمن الحقيقي (يبدأ من 1)
 time_data = np.linspace(1, 10, 500)
 signal_data = np.sin(time_data * 2) + 0.5 * np.sin(time_data * 4) + 0.2 * np.random.normal(0, 1, 500)
