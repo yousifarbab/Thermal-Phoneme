202afbadafce8b6bd050800d0bc23a6e2dfede71
yousifarbab <yousifarbab91@gmail.com>
2026-08-01 16:12:20 +0400

    Create main_app.py

---
+++ main_app.py
@@ -0,0 +1,15 @@
+from thermal_phoneme.phoneme.encoder import (
+    encode_to_phonemes,
+    decode_from_phonemes,
+    encode_to_frequencies,
+    decode_from_frequencies,
+    get_encoding_stats,
+    is_valid_phoneme_text
+)
+from thermal_phoneme.phoneme.phoneme_map import (
+    get_frequency,
+    get_letter,
+    get_all_letters,
+    get_all_frequencies,
+    PHONEME_MAP
+)
